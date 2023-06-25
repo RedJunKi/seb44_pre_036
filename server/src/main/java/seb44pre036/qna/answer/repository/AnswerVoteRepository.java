5e77818fd9cdf6544f270da33cd011a53a031636
RedJunKi <124676153+RedJunKi@users.noreply.github.com>
2023-06-26 01:05:42 +0900

    Delete server directory

--- server/src/main/java/seb44pre036/qna/answer/repository/AnswerVoteRepository.java
+++
@@ -1,7 +0,0 @@
-package seb44pre036.qna.answer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import seb44pre036.qna.answer.entity.AnswerVote;
-
-public interface AnswerVoteRepository extends JpaRepository<AnswerVote,Long> {
-}
--- server/src/main/java/seb44pre036/qna/questionVote/repository/QuestionVoteRepository.java
+++
@@ -1,7 +0,0 @@
-package seb44pre036.qna.questionVote.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import seb44pre036.qna.questionVote.entity.QuestionVote;
-
-public interface QuestionVoteRepository extends JpaRepository<QuestionVote, Long> {
-}
